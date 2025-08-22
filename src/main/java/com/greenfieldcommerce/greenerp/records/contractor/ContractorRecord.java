1f7802cd22ba2b2bfb0ba406c42ebc28546a7101
diegoreidel <diegoreidel@gmail.com>
2025-08-22 08:51:12 -0300

    Remove unused import

--- src/main/java/com/greenfieldcommerce/greenerp/records/contractor/ContractorRecord.java
+++ src/main/java/com/greenfieldcommerce/greenerp/records/contractor/ContractorRecord.java
@@ -1,7 +1,5 @@
 package com.greenfieldcommerce.greenerp.records.contractor;
 
-import java.util.List;
-
 import com.greenfieldcommerce.greenerp.records.contractorrate.ContractorRateRecord;
 
 public record ContractorRecord(Long id, String email, String name, ContractorRateRecord currentRate)
