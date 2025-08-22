dd23b101db42920a41619353530d6a7496dccb05
diegoreidel <diegoreidel@gmail.com>
2025-08-22 09:15:12 -0300

    Remove unused import

--- src/main/java/com/greenfieldcommerce/greenerp/controllers/ContractorsController.java
+++ src/main/java/com/greenfieldcommerce/greenerp/controllers/ContractorsController.java
@@ -2,7 +2,6 @@ package com.greenfieldcommerce.greenerp.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
