8e4e36ef2b5d4541aae5a8902db2c464a222f281
diegoreidel <diegoreidel@gmail.com>
2025-09-30 21:02:59 -0300

    Configuring Spring profiles

--- src/main/java/com/greenfieldcommerce/greenerp/config/AuthenticationLoggingFilter.java
+++ src/main/java/com/greenfieldcommerce/greenerp/config/AuthenticationLoggingFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
+@Profile({"local","dev"})
 public class AuthenticationLoggingFilter extends OncePerRequestFilter
 {
 
