f45d6e136d87f995361b775dad0e6b8a5f3697fa
diegoreidel <diegoreidel@gmail.com>
2025-10-20 19:20:33 -0300

    Fixing the failed tests

--- src/test/java/com/greenfieldcommerce/greenerp/messaging/MessagingConfigTest.java
+++ src/test/java/com/greenfieldcommerce/greenerp/messaging/MessagingConfigTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
+@Import(MessagingConfig.class)
 public class MessagingConfigTest
 {
 	private final RabbitTemplate template;
